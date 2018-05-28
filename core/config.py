92a711f74640ec4cfad0befed30e1a28336f2ded
mrlsdvd <mrlsdvd@DN51vlu4.SUNet>
2018-05-27 18:26:06 -0700

    Add config with relevant paths

---
+++ core/config.py
@@ -0,0 +1,8 @@
+import os, sys
+
+# Relevant paths
+project_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
+models_path = os.path.join(project_path, 'models')
+core_path = os.path.join(project_path, 'core')
+backbones_path = os.path.join(project_path, 'backbones')
+data_path = os.path.join(project_path, 'data')
