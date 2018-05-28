0f763dfc6ee58ebec38548b809d07b238d26d0a3
mrlsdvd <mrlsdvd@DN51vlu4.SUNet>
2018-05-27 19:00:11 -0700

    Add embedding utils for image_captioning model

---
+++ core/models/image_captioning/embedding_utils.py
@@ -0,0 +1,54 @@
+import torch
+import numpy as np
+import pickle
+import os
+import sys
+import argparse
+# Add path to config
+sys.path.append('../../')
+import config as conf
+sys.path.append(os.path.join(conf.backbones_path, 'image_captioning'))
+from build_vocab import Vocabulary
+
+VOCAB_PATH = os.path.join(conf.models_path, 'image_captioning', 'vocab.pkl')
+DECODER_PATH = os.path.join(conf.models_path, 'image_captioning', 'pretrained_model', 'decoder-5-3000.pkl')
+
+def get_embeddings(vocab_path=VOCAB_PATH, decoder_path=DECODER_PATH,
+                   embed_type='numpy'):
+    """Load saved trained embeddings and corresponding vocabulary
+
+    Args:
+        vocab_path (str): path to vocabulary pkl file
+        decoder_path (str): path to trained decoder weights pkl file
+        embed_type (str): whether to return embedding matrix as 'numpy' array or
+                         as torch 'tensor'
+    """
+     # Load vocabulary wrapper
+    with open(vocab_path, 'rb') as f:
+        vocab = pickle.load(f)
+    # Load the trained model parameters
+    embeddings = torch.load(decoder_path)['embed.weight']
+
+    if embed_type == 'numpy':
+        embeddings = embeddings.detach().numpy()
+
+    return embeddings, vocab
+
+
+def main(args):
+    embeddings, vocab = get_embeddings(vocab_path=args.vocab_path,
+                                       decoder_path=args.decoder_path,
+                                       embed_type=args.embed_type)
+    # Check embedding shape
+    print(embeddings.shape)
+    # Check vocabulary
+    print(len(vocab))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--decoder_path', type=str, default=DECODER_PATH, help='path for trained decoder')
+    parser.add_argument('--vocab_path', type=str, default=VOCAB_PATH, help='path for vocabulary wrapper')
+    parser.add_argument('--embed_type', type=str, default='numpy', help='type of embedding matrix')
+    args = parser.parse_args()
+    main(args)
